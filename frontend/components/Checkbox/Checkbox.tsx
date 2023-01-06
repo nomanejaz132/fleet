7ebc13d062871b9c04881a1c1728d2af3f095c74
Noman Ejaz <nomanejaz132@gmail.com>
2023-01-06 23:45:09 +0500

    checkbox

--- frontend/components/Checkbox/Checkbox.tsx
+++ frontend/components/Checkbox/Checkbox.tsx
@@ -15,26 +15,34 @@ export const Checkbox: React.FC<CheckboxProps> = ({ label }) => {
         // intermediate
         id={label}
         aria-label={label}
-        className="w-6 h-6 appearance-none rounded indeterminate:bg-gray-300 checked:bg-blue-500"
+        className="w-6 h-6 appearance-none rounded focus:outline-none focus:ring-0 focus:ring-offset-0 checked:opacity-0 absolute cursor-pointer bg-[#FCFCFD] dark:bg-[#23262F] border-2 border-[#E6E8EC] dark:border-[#353945]"
       />
-      {/* <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M6 12L10 16L18 8"
-          stroke="#FCFCFD"
-          stroke-width="2"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg> */}
+      <div className="w-6 h-6">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="hidden"
+        >
+          <path
+            d="M6 12L10 16L18 8"
+            stroke="#FCFCFD"
+            stroke-width="2"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
 
-      <label htmlFor={label}>{label}</label>
+      <label
+        htmlFor={label}
+        className="text-sm text-[#141416] dark:text-[#FCFCFD] font-medium"
+      >
+        {label}
+      </label>
     </div>
   );
 };
